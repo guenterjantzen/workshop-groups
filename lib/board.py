946c5118a2560716773637a4218f1129ec132150
guenter <guenter.jantzen@gmx.de>
2022-07-28 22:34:21 +0200

    show variante (delta) entfernt

--- lib/board.py
+++ lib/board.py
@@ -66,52 +66,20 @@ class Board:
         return  double_persons, double_pairs
 
     #-----------------------------
-    def show_(self, comment, do_test = False):
-        ok = True
-        if do_test:
-            double_persons, double_pairs = self.test()
-        print (f'<< board {comment}')
-        row = -1
-        n = self.N
-        while row < n-1:
-            row+=1
-            for col in range(n):
-                meeting = self.board.get((row,col))
-                if not meeting:
-                    break
-                print (sorted(meeting), end=' ')
-            if not meeting:
-                break
-            if do_test and double_persons[row]:
-                ok = False
-            if do_test:
-                print(f'  -- {double_persons[row]}')
-            else:
-                print()
-        oktext=''
-        if do_test and double_pairs:
-            ok = False
-            oktext=f'ok={ok}'
-            print(f'{double_pairs}')
-        print(f'row={row}, col={col}{oktext}>>')
-        return ok
-
-  #-----------------------------
     @staticmethod
     def format_meeting(lmeeting):
         s=[f'{person:>2}' for person in lmeeting]
         sep = ' '
         return f'[{sep.join(s)}]'
 
-  #-----------------------------
+    #-----------------------------
     def show(self, comment, do_test = False):
         ok = True
         if do_test:
             double_persons, double_pairs = self.test()
-        print (f'<< board {comment}')
+        #print (f'<< board {comment}')
         row = -1
         n = self.N
-        firstrow = [None]*n
         while row < n-1:
             row+=1
             deltarow = [None]*n
@@ -122,13 +90,6 @@ class Board:
                     lmeeting = [x % n for x in lmeeting]
                 if not lmeeting:
                     break
-                if row == 0:
-                   firstrow[col]=sorted(self.board[(0,col)])
-                else:
-                    #print(f'{row} firstrow[{col}]={firstrow[col]} {firstrow}')
-                    delta = [lmeeting[i]-firstrow[col][i] for i in range(n)]
-                    #deltarow[col]=tuple(sorted(delta))
-                    deltarow[col]=tuple(delta)
                 print (Board.format_meeting(lmeeting), end=' ')
             if not lmeeting:
                 break
@@ -143,9 +104,11 @@ class Board:
                      print (deltarow[col], end=' ')
                 print()
         oktext=''
-        if do_test and double_pairs:
-            ok = False
-            oktext=f'ok={ok}'
-            print(f'{double_pairs}')
-        print(f'row={row}, col={col}{oktext}>>')
+        if do_test:
+            if double_pairs:
+                ok = False
+                oktext=f'ok={ok}'
+                print(f'{double_pairs}')
+            print(f'row={row}, col={col}{oktext}>>')
+        print()
         return ok

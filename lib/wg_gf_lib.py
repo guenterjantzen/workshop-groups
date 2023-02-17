9c376b316eab4ad8decc983846e10c048be4f9dd
guenter <guenter.jantzen@gmx.de>
2023-02-17 19:53:42 +0100

    wg_gf_lib.py: Refactor Mini_GF and indexing

--- lib/wg_gf_lib.py
+++ lib/wg_gf_lib.py
@@ -9,7 +9,57 @@ from galois_field import GFpn
 
 from .board import Board
 
+
+class MiniGF():
+    def __init__(self, els, els_index, sign_list, info):
+        self.els = els
+        self.els_index = els_index
+        self.sign_list = sign_list
+        self.info=info
+        self.n = len(self.els)
+
+    def fill_optables(self):
+        self.op_plus={}
+        self.op_mult={}
+
+        for i, el1 in enumerate(self.els):
+            for j, el2 in enumerate(self.els):
+                i_plus_j=self.els_index[str(el1 + el2)]
+                i_mult_j=self.els_index[str(el1 * el2)]
+                self.op_plus[(i,j)] = i_plus_j
+                self.op_mult[(i,j)] = i_mult_j
+
+
+    def show_optables(self):
+        print (f'\nWorkshop {self.info} +')
+        for i in range(self.n):
+            for j in range(self.n):
+                i_plus_j = self.op_plus[(i,j)]
+                sign = self.sign_list[i_plus_j]
+                print (sign, end=' ')
+            print()
+
+        print (f'\nWorkshop {self.info} *')
+        for i in range(self.n):
+            for j in range(self.n):
+                i_mult_j = self.op_mult[(i,j)]
+                sign = self.sign_list[i_mult_j]
+                print (sign, end=' ')
+            print()
+
+    def __add__(self, other):
+        if isinstance(other, MiniGF):
+            return MiniGF(self.value + other.value)
+
+    def __mul__(self, other):
+        if isinstance(other, MiniGF):
+            return MiniGF(self.value * other.value)
+
+    def __str__(self):
+        return str(self.value)
+
 class SimuGF:
+
     def work(self, basis, power, irr_poly, representation, show='w', verbose=False):
         gf = GFpn(basis, irr_poly)
 
@@ -22,14 +72,17 @@ class SimuGF:
         elif show=='w2':
             self.show = self.show_workshop2
 
-        elToBin={}
-        BinToEl={}
+        self.elToSign={}
+        SignToEl={}
 
-        self.b_list =  []
-        self.b_index = {}
 
-        els_info=[]
-        els=[]
+        self.sign_list =  []
+        self.sign_index = {}
+
+        els_info=[] #kann weg
+        self.els=[]
+        self.els_index = {}
+
         for i in range(basis**power):
             k=i
             l=[]
@@ -40,59 +93,72 @@ class SimuGF:
             while len(l) < power:
                 l.append(0)
             l.reverse()
+
             el = gf.elm(l)
-            els_info.append((l, str(el)))
-            els.append(el)
+            els_info.append((l, str(el))) #kann weg
+            print (l, str(el))
+            self.els.append(el)
+            self.els_index[str(el)] = i
+
             if representation=='b':
-                b=''.join(map(str,l))
+                sign=''.join(map(str,l))
             else:
-                b=str(i)
-            #print (i,b)
-            elToBin[str(el)]=b
-            BinToEl[b]=el
-            self.b_index[b]=i
+                sign=str(i)
+            #print (i,sign)
+            self.elToSign[str(el)] = sign
+            SignToEl[sign] = el
+            self.sign_index[sign]=i
+            self.sign_list.append(sign)
+
+        self.miniGF = miniGF =MiniGF(self.els, self.els_index, self.sign_list, self.info)
+
+        miniGF.fill_optables()
+
 
         if verbose:
-            for i,(b,el) in enumerate(sorted(BinToEl.items())):
-                print('-- ', i,b, str(el))
-        self.show(els, elToBin)
+            for i,(sign,el) in enumerate(sorted(SignToEl.items())):
+                print('-- ', i, sign, str(el))
+        self.show()
 
-    def show_workshop(self, els, elToBin):
+    def show_optables(self):
+        self.miniGF.show_optables()
+
+    def show_workshop(self):
         table={}
-        for h, el_h in enumerate(els):#block
-            for i, el_i in enumerate(els):   #zeile
-                for j, el_j in enumerate(els): #spalte
+        for h, el_h in enumerate(self.els):#block
+            for i, el_i in enumerate(self.els):   #zeile
+                for j, el_j in enumerate(self.els): #spalte
                     if (h,i,j) not in table:
                         el = el_j*el_i + el_h
-                        b=elToBin[str(el)]
-                        table[(h,i,j)] = b
+                        sign =self.elToSign[str(el)]
+                        table[(h,i,j)] = sign
 
         print (f'\nWorkshop {self.info} ')
 
-        n = len(els)
+        n = len(self.els)
         for i in range(n):
             for h in range(n):
                 for j in range(n):
-                    b = table[(h,i,j)]
-                    print(b, end=' ')
+                    sign = table[(h,i,j)]
+                    print(sign, end=' ')
                 print(' | ', end='')
             print()
 
 
-    def show_workshop2(self, els, elToBin):
+    def show_workshop2(self):
         table={}
-        for h, el_h in enumerate(els):#block
-            for i, el_i in enumerate(els):   #zeile
-                for j, el_j in enumerate(els): #spalte
+        for h, el_h in enumerate(self.els):#block
+            for i, el_i in enumerate(self.els):   #zeile
+                for j, el_j in enumerate(self.els): #spalte
                     if (h,i,j) not in table:
                         el = el_j*el_i + el_h
-                        b=elToBin[str(el)]
-                        index_b = self.b_index[b]
+                        sign = self.elToSign[str(el)]
+                        index_b = self.sign_index[sign]
                         table[(h,i,j)] = index_b
 
         print (f'\nWorkshop2 {self.info} ')
 
-        n = len(els)
+        n = len(self.els)
         board = Board(N=n,show_modulo=False)
 
         for row in range(n):
@@ -108,21 +174,6 @@ class SimuGF:
         board.show(comment=';)', do_test=True)
 
 
-    def show_optables(self, els, elToBin):
-        print (f'\nWorkshop {self.info} +')
-        for el1 in els:
-            for el2 in els:
-                el = el1+el2
-                b=elToBin[str(el)]
-                print (b, end=' ')
-            print()
-        print (f'\nWorkshop {self.info} *')
-        for el1 in els:
-            for el2 in els:
-                el = el1*el2
-                b=elToBin[str(el)]
-                print (b, end=' ')
-            print()
 
 #----------------------------
 
